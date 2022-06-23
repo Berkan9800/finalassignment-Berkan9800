6c32734db5c356ddf17b3b43e88847c4e1c20aa9
gurel <berkan.gurel@student.hu.nl>
2022-06-23 06:16:30 +0200

    aanpassing 10

--- src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
+++ src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
@@ -55,7 +55,7 @@ public class BattlesnakeResource {
         int height = (int) request.board.get("height");
         int width = (int) request.board.get("width");
 
-        if (width >= 10) {
+        if (height >= 10) {
             MoveResponse move = new MoveResponse("up", "Going up!");
             return Response.ok(move).build();
 
