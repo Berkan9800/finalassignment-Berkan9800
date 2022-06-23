3d6865b319b67aaa38b5cb7e93725ed4daad205e
gurel <berkan.gurel@student.hu.nl>
2022-06-23 16:14:26 +0200

    aanpassing

--- src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
+++ src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
@@ -64,10 +64,10 @@ public class BattlesnakeResource {
         if (turn == 0) {
             return Response.ok(moveDown).build();
         }
-        if (turn == 2) {
-            return Response.ok(moveUp).build();
+        if (turn == 1) {
+            return Response.ok(moveLeft).build();
         }
-        return Response.ok(moveLeft).build();
+        return Response.ok(moveUp).build();
     }
 
 
