51fd367552253afa6ede5adb0ecb208ce10a9806
gurel <berkan.gurel@student.hu.nl>
2022-06-24 02:02:13 +0200

    aanpassing

--- src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
+++ src/main/java/nl/hu/bep/battlesnake/webservices/BattlesnakeResource.java
@@ -54,30 +54,13 @@ public class BattlesnakeResource {
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
     public Response moveGame(BattlesnakeRequest request) {
-        int height = (int) request.board.get("height");
-        int width = (int) request.board.get("width");
+
         int turn = request.turn;
-
-
-        Object head = request.you.get("head").toString();
-
-        String headString = (String) head;
 
         MoveResponse moveUp = new MoveResponse("up", "going up!");
         MoveResponse moveDown = new MoveResponse("down", "going down!");
         MoveResponse moveLeft = new MoveResponse("left", "going left!");
         MoveResponse moveRight = new MoveResponse("right", "going right!");
-
-
-//        if (headString.equals("{x=0, y=1}") || headString.equals("{x=1, y=1}") || headString.equals("{x=2, y=1}") || headString.equals("{x=3, y=1}") || headString.equals("{x=4, y=1}") || headString.equals("{x=5, y=1}") || headString.equals("{x=6, y=1}") || headString.equals("{x=7, y=1}") || headString.equals("{x=8, y=1}") || headString.equals("{x=9, y=1}") || headString.equals("{x=10, y=1}")){
-//            return Response.ok(moveUp).build();
-//        }
-//
-//        if (headString.equals("{x=0, y=10}") || headString.equals("{x=1, y=10}") || headString.equals("{x=2, y=10}") || headString.equals("{x=3, y=10}") || headString.equals("{x=4, y=10}") || headString.equals("{x=5, y=10}") || headString.equals("{x=6, y=10}") || headString.equals("{x=7, y=10}") || headString.equals("{x=8, y=10}") || headString.equals("{x=9, y=10}") || headString.equals("{x=10, y=10}")){
-//            return Response.ok(moveDown).build();
-//        } else {
-//            return Response.ok(moveUp).build();
-//        }
 
         if (turn < 3) {
             return Response.ok(moveUp).build();
