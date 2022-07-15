0ca21fac325635c07de5a686b2aca9a7869e9ac0
gurel <berkan.gurel@student.hu.nl>
2022-07-15 17:28:40 +0200

    game-service.js aangepast

--- src/main/webapp/games/games-service.js
+++ src/main/webapp/games/games-service.js
@@ -2,7 +2,7 @@ export default class GamesService {
     async getGameIds() {
         //TODO: fetch alle games van de de service, idealiter zonder alle details
         // return Promise.resolve(['dummy-1', 'dummy-2']);
-        return fetch("restservices/snake/end")
+        return fetch("restservices/snake/start")
 
     }
 
